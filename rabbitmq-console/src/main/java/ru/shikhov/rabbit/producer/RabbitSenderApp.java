f4ea83735ef76cd571c8600e837aef6dc3b83672
sAyflort <neefe.saratan.life.life@gmail.com>
2022-11-27 21:13:23 +0300

    Куча hardcode добавлено

--- rabbitmq-console/src/main/java/ru/shikhov/rabbit/producer/RabbitSenderApp.java
+++ rabbitmq-console/src/main/java/ru/shikhov/rabbit/producer/RabbitSenderApp.java
@@ -5,9 +5,13 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 
 public class RabbitSenderApp {
-    private static final String EXCHANGE_NAME = "DoubleDirect";
+    private static final String EXCHANGE_NAME = "ITBlog";
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
@@ -15,11 +19,19 @@ public class RabbitSenderApp {
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
             channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
-
-            channel.basicPublish(EXCHANGE_NAME, "php", null, "php msg".getBytes("UTF-8"));
-            channel.basicPublish(EXCHANGE_NAME, "c++", null, "c++ msg".getBytes("UTF-8"));
-            channel.basicPublish(EXCHANGE_NAME, "java", null, "java msg".getBytes("UTF-8"));
-            System.out.println("OK");
+            while(true) {
+                String str = scanner.nextLine();
+                String topicOrExit = str.split(" ", 2)[0];
+                String msg;
+                if (topicOrExit.equals("exit")) return;
+                if (str.split(" ", 2)[1] == null) {
+                    continue;
+                } else {
+                    msg = str.split(" ", 2)[1];
+                }
+                channel.basicPublish(EXCHANGE_NAME, topicOrExit, null, msg.getBytes("UTF-8"));
+                System.out.println("[*] В тему " + topicOrExit + " отправлено: " + msg);
+            }
         }
     }
 }
